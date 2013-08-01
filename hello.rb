5f0e14a12469fbffd5804fae00a7583f825363d5
Jianqing Peng <pengjianqing@gmail.com>
2013-08-01 10:15:58 +0800

    add hello.rb

---
+++ hello.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby -w
+
+puts "Hello,Ruby!"
+
+#comment
+=begin
+this is multi line comments
+comment line1
+comment line2
+comment line3
+=end
+
+#class
+
+Class Vehicle{
+
+    Function start{
+    puts "start"
+}
+
+Function stop{
+    puts "stop"
+}
+
+}
+
+END{
+    puts "end"
+}
+
+BEGIN{
+    puts "begin"
+
+}
